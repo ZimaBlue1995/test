fda9534f63064e8137c439c1486eb5197c774a37
GitHub Action <action@github.com>
2022-07-06 16:19:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/win15/test/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/win15/test/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.win15.test.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.win15.test.MyBundle
 
 class MyApplicationService {
 
